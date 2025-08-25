157cf36e910a09e4fe54ec296b06db36ea9774b1
Priyanshi <priyanshi.bansal_cs.aiml23@gla.ac.in>
2025-08-25 12:18:15 +0530

    Create Q2.java

---
+++ control_flow/Level1/Q2.java
@@ -0,0 +1,9 @@
+package Level1;
+import java.util.*;
+public class Q2 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        System.out.println("Is the first number the smallest? " + (a < b && a < c));
+    }
+}
