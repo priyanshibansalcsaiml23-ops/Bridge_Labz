a206b65ae4046e52d22db5ccb20a2f49f6cf0c84
Priyanshi <priyanshi.bansal_cs.aiml23@gla.ac.in>
2025-08-25 12:24:58 +0530

    Create Q15.java

---
+++ control_flow/Level1/Q15.java
@@ -0,0 +1,13 @@
+package Level1;
+import java.util.*;
+
+public class Q15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), fact = 1;
+        if(n >= 0) {
+            for(int i = 1; i <= n; i++) fact *= i;
+            System.out.println("Factorial: " + fact);
+        } else System.out.println("Invalid input");
+    }
+}
