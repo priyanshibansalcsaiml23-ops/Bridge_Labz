2705145e15a18b9a8ff5a9e5210a6f849c3d8bcd
Priyanshi <priyanshi.bansal_cs.aiml23@gla.ac.in>
2025-08-25 11:53:47 +0530

    commit

--- eclipse-workspace/JAVACLASS/src/pattern_java/zig-zigtraversal.java
+++ eclipse-workspace/JAVACLASS/src/pattern_java/zig-zigtraversal.java
@@ -1,1 +1,19 @@
+import java.util.*;
+ 
+ public class zig-zagtraversal {
+    public static void zigZagRows(int[][] matrix) {
+        int rows = matrix.length;
+        for (int i = 0; i < rows; i++) {
+            if (i % 2 == 0) {
+                // Left to right
+                for (int j = 0; j < matrix[i].length; j++)
+                    System.out.print(matrix[i][j] + " ");
+            } else {
+                // Right to left
+                for (int j = matrix[i].length - 1; j >= 0; j--)
+                    System.out.print(matrix[i][j] + " ");
+            }
+        }
+    }
+}
 
