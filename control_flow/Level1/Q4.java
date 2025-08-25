b377ea1634725467f9490314ac13327751d24404
Priyanshi <priyanshi.bansal_cs.aiml23@gla.ac.in>
2025-08-25 12:19:47 +0530

    Create Q4.java

---
+++ control_flow/Level1/Q4.java
@@ -0,0 +1,15 @@
+package Level1;
+import java.util.*;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age >= 18)
+            System.out.println("The person's age is " + age + " and can vote.");
+        else
+            System.out.println("The person's age is " + age + " and cannot vote.");
+    }
+
+
+}
