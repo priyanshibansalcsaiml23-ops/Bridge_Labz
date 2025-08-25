3d3ab4bd96cb640d56c6aada7e12f67a8cd07809
Priyanshi <priyanshi.bansal_cs.aiml23@gla.ac.in>
2025-08-25 12:22:23 +0530

    Create Q10.java

---
+++ control_flow/Level1/Q10.java
@@ -0,0 +1,15 @@
+package Level1;
+import java.util.*;
+
+public class Q11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0;
+        while(true) {
+            double num = sc.nextDouble();
+            if(num <= 0) break;
+            total += num;
+        }
+        System.out.println("Total: " + total);
+    }
+}
