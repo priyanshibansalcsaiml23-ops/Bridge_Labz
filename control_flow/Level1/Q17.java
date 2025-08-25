e07826085b12f35d65f1178e20e73d0d4a318b54
Priyanshi <priyanshi.bansal_cs.aiml23@gla.ac.in>
2025-08-25 12:25:59 +0530

    Create Q17.java

---
+++ control_flow/Level1/Q17.java
@@ -0,0 +1,14 @@
+package Level1;
+import java.util.*;
+
+public class Q17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double salary = sc.nextDouble();
+        int years = sc.nextInt();
+        if(years > 5)
+            System.out.println("Bonus: " + (salary * 0.05));
+        else
+            System.out.println("No bonus");
+    }
+}
