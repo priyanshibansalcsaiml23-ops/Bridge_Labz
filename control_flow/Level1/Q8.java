35bc06427c5a4803cccc02fdb1d8c2859df4ec6c
Priyanshi <priyanshi.bansal_cs.aiml23@gla.ac.in>
2025-08-25 12:21:20 +0530

    Create Q8.java

---
+++ control_flow/Level1/Q8.java
@@ -0,0 +1,11 @@
+package Level1;
+import java.util.*;
+
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = sc.nextInt();
+        while(counter >= 1)
+            System.out.println(counter--);
+    }
+}
