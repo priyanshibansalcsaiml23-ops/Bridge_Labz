66839c1faafd7b6ac9fa2c6720fa81f806db1978
Priyanshi <priyanshi.bansal_cs.aiml23@gla.ac.in>
2025-08-25 12:16:47 +0530

    Create Q1.java

---
+++ control_flow/Level1/Q1.java
@@ -0,0 +1,12 @@
+package Level1;
+import java.util.*;
+public class Q1 {   
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    System.out.println("Is the number " + num + " divisible by 5? " + (num % 5 == 0));
+}
+
+
+
+}
